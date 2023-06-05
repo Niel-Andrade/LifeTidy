163c08b1e35817180fa567cecb4b8527c25290d7
Niel-Andrade <nielandrade10@gmail.com>
2023-06-05 19:35:56 -0300

    add modal-tarefa

--- calendar.js
+++ calendar.js
@@ -1,14 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
-  
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      locale: 'pt-br',
-      timeZone: 'UTC',
-      initialView: 'dayGridMonth',
-      events: 'https://fullcalendar.io/api/demo-feeds/events.json',
-      editable: true,
-      selectable: true
-    });
-  
-    calendar.render();
+document.addEventListener('DOMContentLoaded', function () {
+
+  var calendarEl = document.getElementById('calendar');
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    locale: 'pt-br',
+    timeZone: 'UTC',
+    initialView: 'dayGridMonth',
+    events: 'https://fullcalendar.io/api/demo-feeds/events.json',
+    editable: true,
+    selectable: true
   });
+  calendar.render();
+
+
+
+  const buttonNew = document.querySelector(".button-adicionar")
+  const modal = document.querySelector("dialog")
+  const buttonClose = document.querySelector("dialog .dialog-container .div-button-exit i")
+
+  buttonNew.onclick = function () {
+    modal.showModal()
+  }
+  buttonClose.onclick = function(){
+    modal.close()
+  }
+
+});
+
