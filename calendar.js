42a4175a06c6b1d86711c726a51720dc20a89fb5
Niel-Andrade <nielandrade10@gmail.com>
2023-06-05 23:59:13 -0300

    adição do modal de rendimento

--- calendar.js
+++ calendar.js
@@ -14,15 +14,44 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
   const buttonNew = document.querySelector(".button-adicionar")
-  const modal = document.querySelector("dialog")
-  const buttonClose = document.querySelector("dialog .dialog-container .div-button-exit i")
+  const modalTarefa = document.querySelector(".dialog-tarefa")
+  const closeTarefa = document.querySelector(".div-button-exit i")
+  
 
   buttonNew.onclick = function () {
-    modal.showModal()
+    modalTarefa.showModal()
   }
-  buttonClose.onclick = function(){
-    modal.close()
+  closeTarefa.onclick = function(){
+    modalTarefa.close() 
   }
+
+  const buttonInformativo = document.querySelector(".button-desempenho")
+  const modalInformativo = document.querySelector(".dialog-desempenho")
+  const closeDesempenho = modalInformativo.querySelector(".div-button-exit i")
+  buttonInformativo.onclick = function(){
+    modalInformativo.showModal()
+  }
+  closeDesempenho.onclick = function(){
+    modalInformativo.close()
+  }
+
+
+   //lets get the nav tabs
+
+   const tabs = document.querySelectorAll(".nav");
+
+   tabs.forEach((clickedTab) => {
+       //adding click event
+       clickedTab.addEventListener("click", ()=>{
+           //remove current active tab
+           tabs.forEach((nav)=>{
+               nav.classList.remove("active");
+           });
+
+           //switch to next active tab
+           clickedTab.classList.add("active");
+       });
+   });
 
 });
 
